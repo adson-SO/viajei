d4c5d42f613ba84b0a6fd38c9b4c7ae29515e33a
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2022-10-05 11:13:29 -0300

    style: change error string to lowercase

--- src/middlewares/auth.go
+++ src/middlewares/auth.go
@@ -19,7 +19,7 @@ func Auth(c *gin.Context) {
 
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
 		return []byte(os.Getenv("SECRET")), nil
