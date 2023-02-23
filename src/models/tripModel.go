57b6654c13bec1ab43223d81e24453809a8b5e99
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2023-02-23 16:55:46 -0300

    chore: add new fields to trip model

--- src/models/tripModel.go
+++ src/models/tripModel.go
@@ -8,9 +8,12 @@ import (
 
 type Trip struct {
 	gorm.Model
-	Title       string    `json: "title"`
-	Description string    `json: "description"`
-	InitialDate time.Time `json: "initialDate"`
-	EndDate     time.Time `json: "endDate"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	InitialDate time.Time `json:"initialDate"`
+	EndDate     time.Time `json:"endDate"`
+	Price       float64   `json:"price"`
+	Type        string    `json:"type"`
+	Address     string    `json:"address"`
 	UserID      uint
 }
