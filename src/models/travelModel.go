1713c10c8a90c2fcb20d7df0b4296746030ffe2e
Rachel Barros <rachelnmlb@hotmail.com>
2023-04-25 15:19:36 -0300

    chore: adding Photo attribute to travelModel

--- src/models/travelModel.go
+++ src/models/travelModel.go
@@ -13,5 +13,6 @@ type Travel struct {
 	Date        time.Time `json:"date"`
 	Price       float64   `json:"price"`
 	Type        string    `json:"type"`
+	Photo       []byte    `json:"image" gorm:"type:blob"`
 	UserID      uint
 }
