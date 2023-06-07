37cbf75889d1739e97b1457410e712ee43873559
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2023-06-07 14:11:14 -0300

    fix: add cors middleware to allow origin

--- src/routes/router.go
+++ src/routes/router.go
@@ -12,13 +12,13 @@ func LoadRouter() *gin.Engine {
 
 	v1 := router.Group("api/v1")
 	{
-		v1.POST("/signup", controllers.Signup)
-		v1.POST("/signin", controllers.Signin)
-		v1.GET("/user/:id", middleware.Auth, controllers.FindUserById)
-		v1.POST("/travel", middleware.Auth, controllers.CreateTravel)
-		v1.GET("/travel", middleware.Auth, controllers.GetTravels)
-		v1.GET("/travel/:id", middleware.Auth, controllers.GetTravelsById)
-		v1.PUT("/user/reset-password", middleware.Auth, controllers.ResetPassword)
+		v1.POST("/signup", middleware.Cors, controllers.Signup)
+		v1.POST("/signin", middleware.Cors, controllers.Signin)
+		v1.GET("/user/:id", middleware.Cors, middleware.Auth, controllers.FindUserById)
+		v1.POST("/travel", middleware.Cors, middleware.Auth, controllers.CreateTravel)
+		v1.GET("/travel", middleware.Cors, middleware.Auth, controllers.GetTravels)
+		v1.GET("/travel/:id", middleware.Cors, middleware.Auth, controllers.GetTravelsById)
+		v1.PUT("/user/reset-password", middleware.Cors, middleware.Auth, controllers.ResetPassword)
 	}
 
 	return router
