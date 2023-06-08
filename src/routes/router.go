9922bab91f39432dfe333b7f43331a25fea23a95
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2023-06-07 23:25:39 -0300

    fix: fix cors with a new package

--- src/routes/router.go
+++ src/routes/router.go
@@ -4,21 +4,23 @@ import (
 	"api-viajei/src/controllers"
 	middleware "api-viajei/src/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func LoadRouter() *gin.Engine {
 	router := gin.Default()
+	router.Use(cors.Default())
 
 	v1 := router.Group("api/v1")
 	{
-		v1.POST("/signup", middleware.Cors, controllers.Signup)
-		v1.POST("/signin", middleware.Cors, controllers.Signin)
-		v1.GET("/user/:id", middleware.Cors, middleware.Auth, controllers.FindUserById)
-		v1.POST("/travel", middleware.Cors, middleware.Auth, controllers.CreateTravel)
-		v1.GET("/travel", middleware.Cors, middleware.Auth, controllers.GetTravels)
-		v1.GET("/travel/:id", middleware.Cors, middleware.Auth, controllers.GetTravelsById)
-		v1.PUT("/user/reset-password", middleware.Cors, middleware.Auth, controllers.ResetPassword)
+		v1.POST("/signup", controllers.Signup)
+		v1.POST("/signin", controllers.Signin)
+		v1.GET("/user/:id", middleware.Auth, controllers.FindUserById)
+		v1.POST("/travel", middleware.Auth, controllers.CreateTravel)
+		v1.GET("/travel", middleware.Auth, controllers.GetTravels)
+		v1.GET("/travel/:id", middleware.Auth, controllers.GetTravelsById)
+		v1.PUT("/user/reset-password", middleware.Auth, controllers.ResetPassword)
 	}
 
 	return router
